eeb98b0b36ce9fc0076687a73bb7652530f4b0dd
jeffspacer <chris_pope@hotmail.com>
2021-02-09 16:06:11 +0000

    adding head api

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,23 @@
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+
 function Home() {
-  return <div>Welcome, Exlorer!</div>
+  const router = useRouter()
+
+  const handleClick = (e) => {
+    e.preventDefault()
+    router.push('/pokemon')
+  }
+  return (
+    <>
+      <Head>
+        <title>Jamstack espoorts</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+   <div>Welcome, Exlorer!</div>
+   <button onClick={handleClick}>Click me!</button>
+  </>
+  )
 }
 
 export default Home
